ec15c986f04ba4be8d6548061ffd484b68666277
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-09-07 11:59:18 -0500

    update again

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   'host': process.env.HOST,
-  'port': process.env.PORT,
+  'port': 3030,
   'public': '../public/',
   'postgres': process.env.DB_URL,
   'authentication': {
